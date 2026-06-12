1d3f5578b0950a456e274edcf38072dd0e4db653
John6666 <186692226+John6666cat@users.noreply.github.com>
2026-06-12 17:41:56 +0900

    Add files via upload

--- .github/workflows/cuda/Linux-env.sh
+++ .github/workflows/cuda/Linux-env.sh
@@ -5,37 +5,37 @@ case ${1} in
     export CUDA_HOME=/usr/local/cuda-13.0
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0;9.0a;12.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;8.9;9.0;9.0a;12.0+PTX"
     ;;
   cu129)
     export CUDA_HOME=/usr/local/cuda-12.9
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0;9.0a;12.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;8.9;9.0;9.0a;12.0+PTX"
     ;;
   cu128)
     export CUDA_HOME=/usr/local/cuda-12.8
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0;9.0a;12.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;8.9;9.0;9.0a;12.0+PTX"
     ;;
   cu126)
     export CUDA_HOME=/usr/local/cuda-12.6
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;8.9;9.0"
     ;;
   cu124)
     export CUDA_HOME=/usr/local/cuda-12.4
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;8.9;9.0"
     ;;
   cu121)
     export CUDA_HOME=/usr/local/cuda-12.1
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6;8.9;9.0"
     ;;
   *)
     ;;
